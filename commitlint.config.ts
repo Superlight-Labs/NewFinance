755205d24eeb5d42486e85ae6b55be1f459cbe07
Laurenz Honauer <laurenz.honauer@gmail.com>
2023-02-17 16:41:55 +0100

    chore(workspace): setup commitlint rules

---
+++ commitlint.config.ts
@@ -0,0 +1,9 @@
+const scopes = ["app", "api", "workspace"];
+
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-case": [2, "always", "lower-case"],
+    "scope-enum": [2, "always", scopes],
+  },
+};
