165e8d02012288e1a5755885e996f69877bcf27b
Laurenz Honauer <laurenz.honauer@gmail.com>
2023-03-17 13:59:37 +0100

    feat(workspace): group packages in monorepo

--- packages/react-native/rn-crypto-mpc/src/index.tsx
+++ packages/react-native/rn-crypto-mpc/src/index.tsx
@@ -2,7 +2,7 @@ import { Buffer } from 'buffer';
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  "The package 'react-native-blockchain-crypto-mpc' doesn't seem to be linked. Make sure: \n\n" +
+  "The package 'rn-crypto-mpc' doesn't seem to be linked. Make sure: \n\n" +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
