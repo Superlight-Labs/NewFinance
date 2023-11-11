955e95593580e841712d5f68c7c63f71a5131b04
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-11-11 11:58:35 +0100

    style(app): changed headerLargeTitle size

--- apps/mobile/src/screens/menu/menu.stack.tsx
+++ apps/mobile/src/screens/menu/menu.stack.tsx
@@ -20,6 +20,11 @@ const MenuStack = () => {
           headerLargeTitle: true,
           headerShadowVisible: true,
           headerLargeTitleShadowVisible: false,
+          headerLargeTitleStyle: {
+            fontFamily: 'system',
+            fontWeight: '700',
+            fontSize: 32,
+          },
           headerLeft: () => <View />,
         }}
       />
