6858884a041025db794e1d9043e2286fa328e5c6
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-11-16 22:00:17 +0100

    style(app): added round button

--- apps/mobile/src/components/shared/input/button/button.component.tsx
+++ apps/mobile/src/components/shared/input/button/button.component.tsx
@@ -6,17 +6,19 @@ type Props = {
   onPress: () => void;
   children: ReactNode;
   style?: string;
+  textStyle?: string;
   disabled?: boolean;
 };
 
-const Button = ({ onPress, children, style, disabled = false }: Props) => {
+const Button = ({ onPress, children, style, textStyle, disabled = false }: Props) => {
   const bg = disabled ? 'bg-[#606060]' : 'bg-black';
   return (
     <Pressable
       disabled={disabled}
       onPress={onPress}
-      className={`group items-center justify-center rounded px-8 py-3.5 transition-all active:bg-[#000000] ${bg}  ${style}`}>
-      <Text className="group-isolate-active:text-red font-manrope text-sm font-semibold tracking-tight text-white">
+      className={`group items-center justify-center rounded px-2 py-3.5 transition-all active:bg-[#000000] ${bg}  ${style}`}>
+      <Text
+        className={`group-isolate-active:text-red font-manrope text-sm font-bold text-white ${textStyle}`}>
         {children}
       </Text>
     </Pressable>
