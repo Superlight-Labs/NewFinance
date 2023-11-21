150e502987f58cde2a09c10651ed2a9558a1fad7
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-11-21 22:02:48 +0100

    style(app): added button active opacity

--- apps/mobile/src/components/shared/input/button/button.component.tsx
+++ apps/mobile/src/components/shared/input/button/button.component.tsx
@@ -16,7 +16,7 @@ const Button = ({ onPress, children, style, textStyle, disabled = false }: Props
     <Pressable
       disabled={disabled}
       onPress={onPress}
-      className={`group items-center justify-center rounded px-2 py-3.5 transition-all active:bg-[#000000] ${bg}  ${style}`}>
+      className={`group items-center justify-center rounded px-2 py-3.5 transition-all active:opacity-80 ${bg}  ${style}`}>
       <Text
         className={`group-isolate-active:text-red font-manrope text-sm font-bold text-white ${textStyle}`}>
         {children}
