e5b4761c29281f99dbf3c7c5477c3fcc7e449923
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2024-02-12 21:58:10 +0100

    fix(app): fixed current price loading in PriceTextComponent

--- apps/mobile/src/components/shared/price-text/price-text.component.tsx
+++ apps/mobile/src/components/shared/price-text/price-text.component.tsx
@@ -1,10 +1,11 @@
-import useBitcoinPrice from 'hooks/useBitcoinData';
+import { useQuery } from '@tanstack/react-query';
 import { styled } from 'nativewind';
-import { useEffect } from 'react';
 import { currencyItems } from 'screens/menu/pages/currency-settings.screen';
+import { useBitcoinState } from 'state/bitcoin.state';
 import { useGeneralState } from 'state/general.state';
 import { toSatoshi } from 'utils/crypto/bitcoin-value';
 import { formatCurrency } from 'utils/format/format';
+import { backend } from 'utils/superlight-api';
 import { Pressable, Text } from 'utils/wrappers/styled-react-native';
 
 type Props = {
@@ -15,7 +16,18 @@ type Props = {
 
 const PriceText = ({ bitcoinAmount = 0, style, disabled = false }: Props) => {
   const { currency, setCurrency } = useGeneralState();
-  const { currentPrices, updateBitcoinPrice, isLoading } = useBitcoinPrice();
+  const { network } = useBitcoinState();
+
+  const { data: currentExchangeRate } = useQuery(
+    ['exchangeRate'],
+    () =>
+      backend
+        .post<any>('/blockchain/exchange-rate', {
+          network,
+        })
+        .then(res => res.data),
+    { retry: false, refetchInterval: 15000 }
+  );
 
   const changeCurrency = () => {
     const enabledCurrencyItems = currencyItems.filter(item => item.disabled === false);
@@ -24,20 +36,19 @@ const PriceText = ({ bitcoinAmount = 0, style, disabled = false }: Props) => {
     setCurrency(enabledCurrencyItems[nextIndex].value);
   };
 
-  useEffect(() => {
-    updateBitcoinPrice();
-  }, []);
-
   return (
     <Pressable
       disabled={disabled}
       onPress={changeCurrency}
       className="group transition-all active:opacity-70">
       <Text className={`font-manrope text-[40px] font-bold leading-[42px] text-black  ${style}`}>
-        {formatCurrency(
-          currency === 'sats' ? toSatoshi(bitcoinAmount) : bitcoinAmount * currentPrices[currency],
-          currencyItems.find(data => data.value === currency)!.value
-        )}
+        {currentExchangeRate &&
+          formatCurrency(
+            currency === 'sats'
+              ? toSatoshi(bitcoinAmount)
+              : bitcoinAmount * currentExchangeRate.value,
+            currencyItems.find(data => data.value === currency)!.value
+          )}
       </Text>
     </Pressable>
   );
