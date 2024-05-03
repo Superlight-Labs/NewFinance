4c5e40cfb1e64dc06b275c7aaf80f4d2531c1dee
Laurenz Honauer <laurenz.honauer@gmail.com>
2024-05-03 15:47:28 +0200

    refactor(workspace): get rid of rxjs dependency

--- packages/mpc-common/src/websocket/common.d.ts
+++ packages/mpc-common/src/websocket/common.d.ts
@@ -1,13 +1,4 @@
-import { ResultAsync } from 'neverthrow';
-import { Observable } from 'rxjs';
-import { WebsocketError } from '../error';
 import { SignResult } from './results';
-import { MPCWebsocketMessage } from './websocket-messages';
-
-export type MpcWebsocketHandlerWrapper = <T>(
-  handlerResult: Observable<ResultAsync<MPCWebsocketMessage<T>, WebsocketError>>,
-  socket: any
-) => void;
 
 export type WebsocketConfig = {
   baseUrl: string;
--- packages/mpc-common/src/websocket/websocket-messages.d.ts
+++ packages/mpc-common/src/websocket/websocket-messages.d.ts
@@ -1,7 +1,3 @@
-import { ResultAsync } from 'neverthrow';
-import { Observable, Subject } from 'rxjs';
-import { WebsocketError } from './../error';
-
 export type MPCWebsocketMessage<T = string> =
   | MPCWebsocketInProgress
   | { type: 'success'; result: T }
@@ -17,11 +13,3 @@ export type MPCWebscocketInit<T = string> = {
   type: 'init';
   parameter: T;
 };
-
-export type MPCWebsocketResult<T = string> = Observable<
-  ResultAsync<MPCWebsocketMessage<T>, WebsocketError>
->;
-
-export type WebSocketOutput<T = string> = Subject<
-  ResultAsync<MPCWebsocketMessage<T>, WebsocketError>
->;
