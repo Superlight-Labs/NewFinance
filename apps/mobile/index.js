c82f300e7d64649219b75607c6a8ab817743595d
Laurenz Honauer <laurenz.honauer@gmail.com>
2025-03-03 11:27:39 +0100

    refactor(app): get rid of react(-native) hoisting

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,15 +1,9 @@
 /**
  * @format
  */
+import { registerRootComponent } from 'expo';
 import './shim.js';
 
-import { registerRootComponent } from 'expo';
 import App from './src/App';
-
-const appName = {
-  name: 'NewFinance',
-  displayName: 'NewFinance',
-  version: '1.0.0',
-};
 
 registerRootComponent(App);
--- apps/mobile/ios/NewFinance/AppDelegate.mm
+++ apps/mobile/ios/NewFinance/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"NewFinance";
+  self.moduleName = @"main";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
@@ -22,7 +22,7 @@
 - (NSURL *)bundleURL
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"apps/mobile/index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
